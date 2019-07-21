e60c00d2bf05bca27c0329f7490046b4ba0bb58f
Anton Fomkin <ib2006a@yandex.ru>
2019-07-21 11:42:56 +0300

    Промежуточный коммит

---
+++ VkApp/WebView.swift
@@ -0,0 +1,14 @@
+//
+//  WebView.swift
+//  VkApp
+//
+//  Created by Anton Fomkin on 01/07/2019.
+//  Copyright © 2019 Anton Fomkin. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebView: WKWebView {
+
+}
---
+++ VkApp/buttonStyled.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonStyled.swift
+//  VkApp
+//
+//  Created by Anton Fomkin on 19/06/2019.
+//  Copyright © 2019 Anton Fomkin. All rights reserved.
+//
+
+import UIKit
+
+class ButtonStyled: UIButton {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+    }
+
+}
